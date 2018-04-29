d4a91b2e4c9933c901a5916b519747d53ce213b0
Sreeprakash Neelkantan <sree@schogini.com>
2018-04-29 16:27:48 +0530

    First

--- chef1/recipes/file1.rb
+++ chef1/recipes/file1.rb
@@ -1,9 +1,9 @@
 # Run an update on the box.
-execute "apt-get-update" do
+execute "create-file1" do
   command "env > /file1.txt"
 end
 
-cookbook_file "/file1.txt" do
+file "/file1.txt" do
   mode '0644'
   action :touch
   owner 'deploy'
